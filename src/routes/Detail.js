d0f004b2b12334962406193ac2c252589d03f4d9
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2020-09-20 23:14:02 +0900

    no message

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,81 @@
-export default () => "Detail";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+import styled from "styled-components";
+import { TypeNameMetaFieldDef } from "graphql";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      title
+      title_long
+      runtime
+      language
+      medium_cover_image
+      description_intro
+      rating
+    }
+  }
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  background-color: #141414;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+const Poseter = styled.div`
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
+  width: 25%;
+  height: 70%;
+  background-color: transparent;
+`;
+const Title = styled.h1`
+  font-size: 50px;
+`;
+const SubTitle = styled.h4`
+  font-size: 30px;
+  margin-bottom: 10px;
+`;
+const Description = styled.p`
+  font-size: 24px;
+`;
+
+export default () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: parseInt(id) },
+  });
+
+  return (
+    <Container>
+      <Column>
+        <Title>
+          {loading ? "Loading..." : "제목: " + data.movie.title_long}
+        </Title>
+        {!loading && data.movie && (
+          <>
+            <SubTitle>
+              평점: {data.movie.rating}점 런닝타임: {data.movie.runtime}분
+            </SubTitle>
+            <Description>상세정보 : {data.movie.description_intro}</Description>
+          </>
+        )}
+      </Column>
+      <Poseter
+        bg={data && data.movie ? data.movie.medium_cover_image : ""}
+      ></Poseter>
+    </Container>
+  );
+};
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,22 +1,83 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
+import styled from "styled-components";
+import Movie from "../components/Movie";
 
 const GET_MOVIES = gql`
   {
-    movies {
+    movies(rating: 7.0) {
       id
       title
+      medium_cover_image
     }
   }
 `;
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  background-color: #141414;
+`;
+
+const Header = styled.div`
+  background-image: linear-gradient(to right, rgb(142, 14, 0), rgb(31, 28, 24));
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 80px;
+  font-weight: 600;
+  margin-bottom: 20px;
+  color: #e50914;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 30px;
+  color: #141414;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 30px;
+  width: 70%;
+  position: relative;
+  top: -50px;
+`;
+
 export default () => {
-  const { loading, error, data } = useQuery(GET_MOVIES);
-  if (loading) {
-    return "loading..";
-  }
-  if (data && data.movies) {
-    return data.movies.map((m) => <h1>{m.id}</h1>);
-  }
+  const { loading, data } = useQuery(GET_MOVIES);
+  console.log(loading, data);
+  return (
+    <Container>
+      <Header>
+        <Title>NETFLEX</Title>
+        <Subtitle>Mini Version</Subtitle>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+      {!loading && data.movies && (
+        <Movies>
+          {data.movies.map((m) => (
+            <Movie key={m.id} id={m.id} bg={m.medium_cover_image} />
+          ))}
+        </Movies>
+      )}
+    </Container>
+  );
 };
